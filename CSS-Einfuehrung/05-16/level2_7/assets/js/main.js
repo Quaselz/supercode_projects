ed170d69a24afb7a738a391298413a015d525b74
Quaselz <paul.geppert94@freenet.de>
2023-05-17 20:51:50 +0200

    small fix

--- CSS-Einfuehrung/05-16/level2_7/assets/js/main.js
+++ CSS-Einfuehrung/05-16/level2_7/assets/js/main.js
@@ -5,7 +5,7 @@ let counter = 0;
 button.addEventListener("click", (e) => {
 	const newDiv = document.createElement("div");
 	newDiv.textContent = counter;
-	if (!counter % 10) {
+	if (!(counter % 10)) {
 		newDiv.setAttribute("class", "rechteck weiss");
 	} else {
 		newDiv.setAttribute("class", "rechteck");
