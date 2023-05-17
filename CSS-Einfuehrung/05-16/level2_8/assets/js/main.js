0db85d282fba5b080ee412d7191939e75346ff61
Quaselz <paul.geppert94@freenet.de>
2023-05-17 21:06:05 +0200

    bonus aufgabe von 2_8 am 16_05

--- CSS-Einfuehrung/05-16/level2_8/assets/js/main.js
+++ CSS-Einfuehrung/05-16/level2_8/assets/js/main.js
@@ -10,3 +10,14 @@ submitButton.addEventListener("click", () => {
 	newLi.appendChild(text);
 	outputUl.appendChild(newLi);
 });
+document.addEventListener("keydown", (e) => {
+	console.log(e.which);
+	if (e.which === 13) {
+		//const userInputText = document.querySelector("#userinput").value;
+		const newLi = document.createElement("li");
+		const text = "ich wurde durch enter erstellt.";
+		const textNode = document.createTextNode(text);
+		newLi.appendChild(textNode);
+		outputUl.appendChild(newLi);
+	}
+});
